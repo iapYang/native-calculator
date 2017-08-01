246f142b01a7ffd31ac94d369ed706454deaf19d
DESKTOP-8IQMI96\Creasy <yklorse@gmail.com>
2017-08-01 10:27:58 +0800

    finish struct

--- dev/component/App.vue
+++ dev/component/App.vue
@@ -11,6 +11,24 @@
 export default {};
 </script>
 
-<style lang="scss" scoped>
-    
+<style lang="scss">
+@import '../style/reset';
+html,
+body {
+    width: 100%;
+    height: 100%;
+}
+#app, .app-container {
+    width: 100%;
+    height: 100%;
+}
+.calculator {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    width: 375px;
+    height: 560px;
+    background-color: #000;
+}
 </style>
